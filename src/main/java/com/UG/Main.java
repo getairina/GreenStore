b461718b37cf913cadf33a5caaabdb05a68eaaec
Geta Ureche <getairina@yahoo.com>
2019-10-29 20:00:37 +0200

    ultima varianta

--- src/main/java/com/UG/Main.java
+++ src/main/java/com/UG/Main.java
@@ -1,5 +1,6 @@
 package com.UG;
 
+import com.UG.controller.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,19 +12,24 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Main extends Application {
+    Controller controller;
+
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GreenStore");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(getClass().getClassLoader().getResourceAsStream("main.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
         primaryStage.setTitle("GreenStore"); // title of window
         primaryStage.setScene(new Scene(root, 600, 800));
         primaryStage.setMinHeight(800);
         primaryStage.setMinWidth(600);
+
+        controller = fxmlLoader.getController();
 
         //without lambda
 //        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
